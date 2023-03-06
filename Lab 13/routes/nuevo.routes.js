8cc6992e9bcb4052d69f0d8f1eba5b418066b14c
Alan Patricio González Bernal <alanpagzz@MacBook-Pro-de-Alan.local>
2023-03-06 09:57:08 -0600

    Se empieza lab 13

---
+++ Lab 13/routes/nuevo.routes.js
@@ -0,0 +1,12 @@
+//Basandonos en el que tiene el profe sobre perros
+const express = require('express');
+
+const router = express.Router();
+
+
+const nuevoController = require('../controllers/nuevo.controller');
+
+router.get('/', nuevoController);
+                        
+
+module.exports = router;
